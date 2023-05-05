7eac09d344c29b01fdb20bb2e2c811f7c30ccc77
ljmacnee <l.macnee@outlook.com>
2023-05-05 16:46:10 +1000

    Budget GPT created.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Import necessary libraries
 import openai
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import config
 
 # Set up OpenAI API key
@@ -8,6 +8,9 @@ openai.api_key = config.API_KEY
 
 # Initialize Flask app
 app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 # Define endpoint for chatbot
 @app.route('/chatbot', methods=['POST'])
@@ -15,25 +18,29 @@ def chatbot():
     # Get user input from request
     input_text = request.json['text']
 
-    # Set up OpenAI GPT-3 parameters
-    model_engine = "text-davinci-002"
-    prompt = f"User: {input_text}\nChatbot:"
+    prompt = f"{input_text}"
 
     # Use OpenAI API to generate response
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": prompt},
+        ]
     )
 
     # Extract chatbot response from OpenAI API response
-    chatbot_response = response.choices[0].text.strip()
+    chatbot_response = response.choices[0].message.get("content")
 
-    # Return chatbot response as JSON
-    return jsonify({'response': chatbot_response})
+    # Call your other API here with the prompt parameter
+    # and store the response in the `other_api_response` variable
+    other_api_response = "Your other API response here"
+
+    # Return both responses as JSON
+    return jsonify({
+        'chatbot_response': chatbot_response,
+        'other_api_response': other_api_response
+    })
 
 # Run Flask app
 if __name__ == '__main__':
